b2db29da95444fa5334039af483c854a6dfcba3f
AntRod18 <antrod1895@gmail.com>
2021-02-04 14:26:36 -0500

    commit tree b106162b3e0f66a1e8b658df61621119f9310b2c

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+def prime?(integer)
+    i = 2
+    if integer > 1
+        while i < integer
+            return false if integer % i == 0
+            i += 1
+        end
+        return true
+    end
+    return false
+
+end
